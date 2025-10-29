6327ff924daed4e2e008e2a0b7bc7286239bf8b8
rez <zero.workspace@gmail.com>
2025-10-29 19:29:07 +0330

    EDA: adding date variables to dataset

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -5,14 +5,31 @@ import seaborn as sns
 
 df = pd.read_csv('../DataSet/Superstore.csv', encoding='latin1')
 
-print(df.head())
+# print(df.head())
+#
+# print(df.shape)
+#
+# print(df.describe())
+#
+# print(df.info())
+#
+# print(df.isnull().sum())
 
-print(df.shape)
+df['Order Date'] = pd.to_datetime(df['Order Date'])
 
-print(df.describe())
+df['Ship Date'] = pd.to_datetime(df['Ship Date'])
 
-print(df.info())
+df['Year'] = df['Order Date'].dt.year
 
-print(df.isnull().sum())
+df['Month'] = df['Order Date'].dt.month
 
+df['Month_Name'] = df['Order Date'].dt.month_name()
+
+df['Quarter'] = df['Order Date'].dt.quarter
+
+df['Day_of_Week'] = df['Order Date'].dt.day_name()
+
+df['Week_of_Year'] = df['Order Date'].dt.isocalendar().week
+
+print(df[['Order Date', 'Year', 'Month', 'Month_Name', 'Quarter', 'Day_of_Week']].head(10))
 
