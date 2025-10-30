d787b380f91ad09266e2eaf3a6d2376275c2f615
rez <zero.workspace@gmail.com>
2025-10-30 16:11:54 +0330

    KPI:mplemented base KPI calculations and time-based feature set

--- KPIs/Basic.py
+++ KPIs/Basic.py
@@ -24,4 +24,43 @@ kpi_summary = pd.DataFrame({
     'Value': [f"${total_sales:,.2f}", f"${total_profit:,.2f}", f"{avg_discount:.2%}", f"${aov:,.2f}", f"{profit_margin:.2f}%"]
 })
 
-print(kpi_summary)
+# print(kpi_summary)
+
+region_kpi = df.groupby('Region').agg({
+    'Sales': 'sum',
+    'Profit': 'sum',
+    'Discount': 'mean',
+    'Order ID': pd.Series.nunique
+}).reset_index()
+
+region_kpi['AOV'] = region_kpi['Sales'] / region_kpi['Order ID']
+region_kpi['Profit Margin (%)'] = (region_kpi['Profit'] / region_kpi['Sales']) * 100
+region_kpi.rename(columns={'Discount': 'Avg Discount'}, inplace=True)
+
+# print(region_kpi)
+
+category_kpi = df.groupby('Category').agg({
+    'Sales': 'sum',
+    'Profit': 'sum',
+    'Discount': 'mean',
+    'Order ID': pd.Series.nunique
+}).reset_index()
+
+category_kpi['AOV'] = category_kpi['Sales'] / category_kpi['Order ID']
+category_kpi['Profit Margin (%)'] = (category_kpi['Profit'] / category_kpi['Sales']) * 100
+category_kpi.rename(columns={'Discount': 'Avg Discount'}, inplace=True)
+
+# print(category_kpi)
+
+segment_kpi = df.groupby('Segment').agg({
+    'Sales': 'sum',
+    'Profit': 'sum',
+    'Discount': 'mean',
+    'Order ID': pd.Series.nunique
+}).reset_index()
+
+segment_kpi['AOV'] = segment_kpi['Sales'] / segment_kpi['Order ID']
+segment_kpi['Profit Margin (%)'] = (segment_kpi['Profit'] / segment_kpi['Sales']) * 100
+segment_kpi.rename(columns={'Discount': 'Avg Discount'}, inplace=True)
+
+# print(segment_kpi)
